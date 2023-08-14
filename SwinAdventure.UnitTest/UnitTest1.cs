c85712765ed014757368a8b6762168164f60f97e
William <59117351+Mierln@users.noreply.github.com>
2023-08-14 22:54:53 +1000

    Adding NUnitTest

---
+++ SwinAdventure.UnitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace SwinAdventure.UnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        IdentifiableObject _testObject = new IdentifiableObject();
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
