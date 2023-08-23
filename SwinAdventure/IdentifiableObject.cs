f8850f27518a2545287cf9b647fb9aa6459fcda2
William <59117351+Mierln@users.noreply.github.com>
2023-08-24 00:49:52 +1000

    removed comments

--- SwinAdventure/IdentifiableObject.cs
+++ SwinAdventure/IdentifiableObject.cs
@@ -3,10 +3,9 @@ namespace SwinAdventure
 {
     public class IdentifiableObject
     {
-        // private Variables
         List<string> _identifiers;
 
-        // Adds identifiers to the Identifiable Object from the passed in array
+
         public IdentifiableObject(string[] idents)
         {
             _identifiers = new List<string>();
@@ -17,13 +16,13 @@ namespace SwinAdventure
             }
         }
 
-        // Checks if the passed in identifier is in the _identifiers
+
         public bool AreYou(string id)
         {
             return _identifiers.Contains(id.ToLower());
         }
 
-        // Returns the first identifier from _identifiers, or an empty string if the object has no identifiers
+
         public string FirstId
         {
             get
@@ -39,7 +38,7 @@ namespace SwinAdventure
             }
         }
 
-        // Converts the identifier to lower case and stores it in _identifiers
+
         public void AddIdentifier(string id)
         {
             _identifiers.Add(id.ToLower());
