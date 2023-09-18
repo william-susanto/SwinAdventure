d4a68c8eb8a628862f10e7a2a7deb188b5ed952d
William <59117351+Mierln@users.noreply.github.com>
2023-09-18 10:50:49 +1000

    5.2

--- SwinAdventure.UnitTest/BagTest.cs
+++ SwinAdventure.UnitTest/BagTest.cs
@@ -3,34 +3,39 @@
 namespace SwinAdventure.UnitTest;
 
 [TestFixture]
-public class PlayerTest
+public class BagTest
 {
-    private Player _testObject1;
+    private Bag _testBag1;
+    private Bag _testBag2;
+
     private Item _newItem1;
     private Item _newItem2;
+    private Item _newItem3;
 
     [SetUp]
     public void Setup()
     {
-        _testObject1 = new Player("William", "Just another player in this world");
+        _testBag1 = new Bag(new string[] { "self", "backpack" }, "brown backpack", "A short sword cast from bronze");
+        _testBag2 = new Bag(new string[] { "self", "bag" }, "mini bag", "A short sword cast from bronze");
 
         _newItem1 = new Item(new string[] { "sword", "bronze" }, "bronze sword", "A short sword cast from bronze");
         _newItem2 = new Item(new string[] { "shovel", "spade" }, "shovel", "This is a might fine shovel");
+        _newItem3 = new Item(new string[] { "water", "bottle" }, "water bottle", "Carry water with this item");
 
     }
 
     [Test]
-    public void TestPlayerIdentifiable()
+    public void TestBagLocatesItem()
     {
-        Assert.True(_testObject1.AreYou("me"));
+        Assert.That(_testBag1.Locate("shovel"), Is.EqualTo(_newItem2));
     }
 
-    //[Test]
-    //public void TestPlayerLocatesItem()
-    //{
-    //    Assert.That(_testObject1.Locate("me"), Is.EqualTo(_testObject1));
-        
-    //}
+    [Test]
+    public void TestBagLocatesItself()
+    {
+        Assert.That(_testBag1.Locate("self"), Is.EqualTo(_testBag1));
+
+    }
 
     //[Test]
     //public void TestPlayerLocatesItself()
